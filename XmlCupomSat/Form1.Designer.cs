9ff9b1f24bdd5bcccb9ba4c295ad3e29daa45f08
Luiz Felipe carlos <felippz@icloud.com>
2021-07-06 21:43:52 -0300

    Subindo projeto

---
+++ XmlCupomSat/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace XmlCupomSat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImporta = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCaminho = new System.Windows.Forms.TextBox();
+            this.btnSelectCaminho = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtInstancia = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtBanco = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.dtpDataDe = new System.Windows.Forms.DateTimePicker();
+            this.dtpDataAte = new System.Windows.Forms.DateTimePicker();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnImporta
+            // 
+            this.btnImporta.Location = new System.Drawing.Point(10, 225);
+            this.btnImporta.Name = "btnImporta";
+            this.btnImporta.Size = new System.Drawing.Size(75, 23);
+            this.btnImporta.TabIndex = 6;
+            this.btnImporta.Text = "Importa";
+            this.btnImporta.UseVisualStyleBackColor = true;
+            this.btnImporta.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCaminho);
+            this.groupBox1.Controls.Add(this.btnSelectCaminho);
+            this.groupBox1.Location = new System.Drawing.Point(10, 170);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(375, 45);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pasta do Arquivo";
+            // 
+            // txtCaminho
+            // 
+            this.txtCaminho.Location = new System.Drawing.Point(6, 19);
+            this.txtCaminho.Name = "txtCaminho";
+            this.txtCaminho.Size = new System.Drawing.Size(313, 20);
+            this.txtCaminho.TabIndex = 0;
+            // 
+            // btnSelectCaminho
+            // 
+            this.btnSelectCaminho.Location = new System.Drawing.Point(325, 17);
+            this.btnSelectCaminho.Name = "btnSelectCaminho";
+            this.btnSelectCaminho.Size = new System.Drawing.Size(44, 23);
+            this.btnSelectCaminho.TabIndex = 0;
+            this.btnSelectCaminho.Text = "...";
+            this.btnSelectCaminho.UseVisualStyleBackColor = true;
+            this.btnSelectCaminho.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtInstancia);
+            this.groupBox2.Location = new System.Drawing.Point(10, 58);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(182, 45);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Instâncias do Banco de Dados";
+            // 
+            // txtInstancia
+            // 
+            this.txtInstancia.Location = new System.Drawing.Point(6, 19);
+            this.txtInstancia.Name = "txtInstancia";
+            this.txtInstancia.Size = new System.Drawing.Size(166, 20);
+            this.txtInstancia.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtBanco);
+            this.groupBox3.Location = new System.Drawing.Point(203, 58);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(182, 45);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Nome do Banco  de Dados";
+            // 
+            // txtBanco
+            // 
+            this.txtBanco.Location = new System.Drawing.Point(6, 19);
+            this.txtBanco.Name = "txtBanco";
+            this.txtBanco.Size = new System.Drawing.Size(166, 20);
+            this.txtBanco.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtSenha);
+            this.groupBox4.Location = new System.Drawing.Point(203, 109);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(182, 45);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(6, 19);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(166, 20);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.txtId);
+            this.groupBox5.Location = new System.Drawing.Point(10, 109);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(182, 45);
+            this.groupBox5.TabIndex = 3;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Usuário";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(6, 19);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(166, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.txtSerie);
+            this.groupBox6.Location = new System.Drawing.Point(10, 7);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(62, 45);
+            this.groupBox6.TabIndex = 0;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Série NF";
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.Location = new System.Drawing.Point(6, 19);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(49, 20);
+            this.txtSerie.TabIndex = 0;
+            // 
+            // dtpDataDe
+            // 
+            this.dtpDataDe.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataDe.Location = new System.Drawing.Point(45, 16);
+            this.dtpDataDe.Name = "dtpDataDe";
+            this.dtpDataDe.Size = new System.Drawing.Size(104, 20);
+            this.dtpDataDe.TabIndex = 7;
+            // 
+            // dtpDataAte
+            // 
+            this.dtpDataAte.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataAte.Location = new System.Drawing.Point(193, 16);
+            this.dtpDataAte.Name = "dtpDataAte";
+            this.dtpDataAte.Size = new System.Drawing.Size(104, 20);
+            this.dtpDataAte.TabIndex = 8;
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.label2);
+            this.groupBox7.Controls.Add(this.label1);
+            this.groupBox7.Controls.Add(this.dtpDataDe);
+            this.groupBox7.Controls.Add(this.dtpDataAte);
+            this.groupBox7.Location = new System.Drawing.Point(78, 7);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(307, 45);
+            this.groupBox7.TabIndex = 9;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Até";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "De";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 260);
+            this.Controls.Add(this.groupBox7);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnImporta);
+            this.Name = "Form1";
+            this.Text = "Importador de XML";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImporta;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCaminho;
+        private System.Windows.Forms.Button btnSelectCaminho;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtInstancia;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtBanco;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.DateTimePicker dtpDataDe;
+        private System.Windows.Forms.DateTimePicker dtpDataAte;
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ XmlCupomSat/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace XmlCupomSat
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection con;
+
+        #region Metodos Privados
+        #endregion
+        private void conexao(string instancia, string banco, string usuario, string senha)
+        {
+            con = new SqlConnection(string.Format(
+                    @"Data Source = {0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3}", instancia, banco, usuario, senha));
+            try
+            {
+                con.Open();
+            }
+            catch (Exception er)
+            {
+
+                MessageBox.Show(er.Message);
+            }
+
+        }
+        private void GravaArquivo(string pastaSalva, string nomeArquivo, string xml)
+        {
+            try
+            {
+                //Escreve ou cria Arquivo
+                string EndAplicacao = pastaSalva + string.Format(@"\CF-e{0}.XML", nomeArquivo);
+                StreamWriter ArquivoConfig = new StreamWriter(EndAplicacao);
+                string Dados = string.Format("{0}", xml);
+                ArquivoConfig.WriteLine(Dados);
+                ArquivoConfig.Close();
+            }
+            catch (Exception Erro)
+            {
+                MessageBox.Show(Erro.Message);
+            }
+        }
+        private void ExecutaGrava(string serie, DateTime dataInicio, DateTime dataFim)
+        {
+            string dataIni = dataInicio.Date.ToString("yyyyMMdd");
+            string dataF = dataFim.Date.ToString("yyyyMMdd");
+            SqlCommand dc = new SqlCommand();
+            try
+            {
+
+                dc.CommandText = string.Format(@"select nc.serie, nc.status, nc.xmlaprova, nc.xmlcancela, nc.numero 
+from NFiscal as nf inner join Nfiscalcontrole nc on nf.NumNF = nc.Numero and nf.Series = nc.Serie
+where nc.Serie = '{0}' and nf.DataNota between '{1}' and '{2} 23:59:59:999'", serie, dataIni, dataF);
+                dc.CommandType = CommandType.Text;
+                dc.Connection = con;
+            }
+            catch (Exception z)
+            {
+                MessageBox.Show(z.Message);
+            }
+
+            SqlDataReader rd;
+            rd = dc.ExecuteReader();
+
+
+            while (rd.Read())
+            {
+                if (rd["status"].ToString() == "C" && rd["XmlCancela"].ToString() != "")
+                {
+                    GravaArquivo(txtCaminho.Text, "Canc"+ rd["Numero"].ToString(), rd["XmlCancela"].ToString());
+                }
+                else if (rd["status"].ToString() == "I" && rd["XmlAprova"].ToString() != "")
+                {
+                    GravaArquivo(txtCaminho.Text, rd["Numero"].ToString(), rd["XmlAprova"].ToString());
+                }
+            }
+            MessageBox.Show("Importação Finalizada com sucesso!");
+        }
+        private void FechaConexao()
+        {
+            con.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region Verifica parametros Banco
+            if (txtInstancia.Text != "")
+            {
+                if (txtBanco.Text != "")
+                {
+                    if (txtId.Text != "")
+                    {
+                        conexao(txtInstancia.Text, txtBanco.Text, txtId.Text, txtSenha.Text);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Informe o Usuario");
+                        txtId.Focus();
+                        return;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Informe a Base de Dados");
+                    txtBanco.Focus();
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe o caminnho da Base de Dados");
+                txtInstancia.Focus();
+                return;
+            }
+            #endregion
+            #region Verifica conexão aberta e se o caminho destino foi informado
+            if (con.State == ConnectionState.Open)
+            {
+                if (txtCaminho.Text != "")
+                {
+                    ExecutaGrava(txtSerie.Text, dtpDataDe.Value, dtpDataAte.Value);
+                }
+                else
+                {
+                    txtCaminho.Focus();
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Erro na conexão com a Base de Dados");
+            }
+            #endregion
+        }
+
+
+
+        #region Caminho da pasta
+        #endregion
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog pasta = new FolderBrowserDialog();
+            if (pasta.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtCaminho.Text = pasta.SelectedPath;
+            }
+        }
+    }
+}
